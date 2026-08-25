a30c06955cb8a3221026fd4e89d19a8d342c5e2e
sebastienrousseau <sebastienrousseau@users.noreply.github.com>
2026-08-25 16:31:19 +0000

    docs: deploy documentation [skip ci] 1f2a34077e94aba878b3ccc102429b53e40ab021

--- trait.impl/log/trait.Log.js
+++ trait.impl/log/trait.Log.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["commons",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.33/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"commons/logging/logger/struct.LoggingFacade.html\" title=\"struct commons::logging::logger::LoggingFacade\">LoggingFacade</a>",0]]]]);
+    const implementors = Object.fromEntries([["commons",[["impl <a class=\"trait\" href=\"https://docs.rs/log/0.4.34/log/trait.Log.html\" title=\"trait log::Log\">Log</a> for <a class=\"struct\" href=\"commons/logging/logger/struct.LoggingFacade.html\" title=\"struct commons::logging::logger::LoggingFacade\">LoggingFacade</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
